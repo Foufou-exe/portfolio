de6afad940746ededc8c7c673bce6c4a5b0db02b
Foufou.exe <thibaut.maurras34@gmail.com>
2026-02-03 20:18:27 +0100

    feat: ajouter le composant Speed Insights de Vercel pour l'analyse des performances

--- app/app.vue
+++ app/app.vue
@@ -10,9 +10,13 @@
       />
     </NuxtLayout>
   </div>
+  <SpeedInsights />
 </template>
 
 <script lang="ts" setup>
+// sonner toast notifications
 import 'vue-sonner/style.css'
 import { Toaster } from '~/components/ui/sonner'
+// analytics component for Vercel Speed Insights
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
